e3d2b75c7b2a15b4fd97e58b5f343e91335e9f65
Daniel Matney <danieljmatney@gmail.com>
2019-09-03 00:37:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,4 +26,10 @@ def using_concat(array1, array2)
  array1.concat(array2)
 end
 
-using_insert
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
